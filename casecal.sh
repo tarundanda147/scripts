c5c25e2710f6f7dc4bb85df30f203142daa4a806
tarundanda147 <154943106+tarundanda147@users.noreply.github.com>
2024-01-11 11:38:26 +0530

    Update casecal.sh

--- casecal.sh
+++ casecal.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
-echo "enter first number"
-read num1
-echo "enter second number"
-read num2
-echo "enter operator + to add, - to substract, * to multiply, / division and % to get reminder"
-read oper
-case $oper in
-	'+')sum=`expr $num1 + $num2`
+#echo "enter first number"
+#read num1
+#echo "enter second number"
+#read num2
+#echo "enter operator + to add, - to substract, * to multiply, / division and % to get reminder"
+#read oper
+case $3 in
+	'+')sum=`expr $1 + $2`
 		echo "sum of two numbers:$sum"
 		;;
-	'-')diff=`expr $num1 - $num2`
+	'-')diff=`expr $1 - $2`
 		echo "difference of two numbers:$diff"
 		;;
-	'*')mult=`expr $num1 \* $num2`
+	'*')mult=`expr $1 \* $2`
 		echo "product of two numbers:$mult"
 		;;
-	'/')if [ $num2 -eq 0 ]
+	'/')if [ $2 -eq 0 ]
 		then
 			echo "division of two numbers is infinity"
 		else
-			divi=`expr $num1 / $num2`
+			divi=`expr $1 / $2`
 		echo " division of two numbers:$divi"
 	fi
 		;;
-	'%')remi=`expr $num1 % $num2`
+	'%')remi=`expr $1 % $2`
 		echo "reminder of two numbers:$remi"
 		;;
 esac
